98a085f4adb613ad22cfd72d2082075a7c579157
dylanejosse <dylane.josse@oclock.school>
2023-08-21 20:19:20 +0200

    Initialization of the access control

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -12,6 +12,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * Require ROLE_USER for all the actions of this controller
+ *
+ * @IsGranted("ROLE_USER")
+ */
 
 class ProjectController extends AbstractController
 {
