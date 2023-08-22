44c9678045b3b35c284e77dd9d08072911157641
dylanejosse <dylane.josse@oclock.school>
2023-08-22 23:13:32 +0200

    Initialization of the EditCurrentUser Form

--- src/Controller/CurrentUserController.php
+++ src/Controller/CurrentUserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\EditCurrentUserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class CurrentUserController extends AbstractController
 {
@@ -25,13 +28,53 @@ class CurrentUserController extends AbstractController
     /**
      * @Route("/mes-informations/delete", name="app_current_user_delete")
      */
-    public function delete(UserRepository $userRepository, Request $request): Response
+    public function delete(UserRepository $userRepository, Request $request, TokenStorageInterface $tokenStorage): Response
     {
         $currentUser = $this->getUser();
         $userRepository->remove($currentUser, true);
 
-        // This one is needed to kill the session's user before the redirection
+        // This part is needed to kill the session's user before the redirection
         $request->getSession()->invalidate();
+        $this->container->get("security.token_storage")->setToken(null);
+
         return $this->redirectToRoute('app_home');
+    }
+
+    /**
+     * @Route("/mes-informations/edit", name="app_current_user_edit")
+     */
+    public function edit(UserRepository $userRepository, Request $request, EntityManagerInterface $em): Response
+    {
+        $currentUserId = $this->getUser()->getId();
+        
+        $currentUser = $userRepository->find(["id" => $currentUserId]);
+        
+
+        $form = $this->createForm(EditCurrentUserType::class, $currentUser);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data = $form->getData();
+
+            $currentUser->setUsername($data->getUsername());
+            $currentUser->setStudyLevel($data->getStudyLevel());
+            $currentUser->setTimeAvailableWeek($data->getTimeAvailableWeek());
+
+            foreach ($data->getTechnologies() as $technology)
+            {
+                $currentUser->addTechnology($technology);
+            };
+
+            $em->flush($currentUser);
+
+            return $this->redirectToRoute("app_current_user_informations");
+            
+        }
+
+        return $this->render('current_user/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/EditCurrentUserType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Technology;
+use App\Entity\User;
+use App\Repository\TechnologyRepository;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditCurrentUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => "Votre nom d'utilisateur"
+            ])
+            ->add('study_level', ChoiceType::class, [
+                'label' => "Votre niveau d'études",
+                'choices' => [
+                    "Autodidacte" => "Autodidacte",
+                    "Bac + 2" => "Bac + 2",
+                    "Bac + 3" => "Bac + 3",
+                    "Bac + 5" => "Bac + 5"
+                ]
+                ])
+            
+            ->add('time_available_week', ChoiceType::class, [
+            'label' => 'Estimation du temps que vous souhaitez consacrer à des projets tech par semaine',
+            'choices' => [
+                "Moins de 5 heures" => "Moins de 5 heures",
+                "Entre 5 et 15 heures" => "Entre 5 et 15 heures",
+                "Entre 15 et 30 heures" => "Entre 15 et 30 heures",
+                "Plus de 30 heures" => "Plus de 30 heures"
+                ]
+            ])
+
+            ->add('technologies', EntityType::class, [
+                'class' => Technology::class,
+                // Sort all technologies by name
+                'query_builder' => function (TechnologyRepository $tr): QueryBuilder {
+                    return $tr->createQueryBuilder('t')
+                        ->orderBy('t.name', 'ASC');
+                        },
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'label' => 'Technologies sur lesuqelles vous souhaitez travailler',
+                ])
+
+            ->add('Modifier', SubmitType::class, [
+                "attr" => [
+                    "class" => "btn btn-warning"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
