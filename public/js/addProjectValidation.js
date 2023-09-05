cd09fe87cb119f400e139e7cc7c77cbf9f5bf5ae
dylanejosse <dylane.josse@oclock.school>
2023-09-05 22:40:49 +0200

    AddProjectForm : first validation with javascript

--- public/js/addProjectValidation.js
+++ public/js/addProjectValidation.js
@@ -5,9 +5,18 @@ addProjectForm.addEventListener("submit", function(e)
     let errors = document.querySelector(".errors");
     errors.innerHTML = "";
 
+    //TODO: Ajouter la vérification sur les checkbox
+
     let inputName = document.querySelector("#add_project_name");
     let inputSummary = document.querySelector("#add_project_summary");
-    let regexName = /^[a-zA-Z0-9-\s]$/
+    let inputDescription = document.querySelector("#add_project_description");
+    let inputTechnologies = document.querySelector("#add_project_technologies");
+    let inputProjectImage = document.querySelector("#add_project_image");
+    let inputDuration = document.querySelector("#add_project_duration");
+    let inputStudyLevel = document.querySelector("#add_project_study_level");
+    let inputNeed = document.querySelector("#add_project_need_description");
+    let inputTimeWeek = document.querySelector("#add_project_time_necessary_week");
+    let regexName = /^[\w\s]+/
 
     // trim() permet de supprimer les espaces avant et après l'entrée de l'utilisateur
     if (inputName.value.trim() === "")
@@ -17,10 +26,10 @@ addProjectForm.addEventListener("submit", function(e)
         errors.appendChild(error);
         e.preventDefault();
     } 
-    else if (!regexName.test(inputName.value))
+    else if (regexName.test(inputName.value.trim()) === false)
     {
         let error = document.createElement("p");
-        error.textContent = "Pour le nom du projet, seuls les caractères suivants sont autorisés : [a-z] [A-Z] [0-9] [éèàç+'ù-_]";
+        error.textContent = "Pour le nom du projet, seuls les caractères suivants sont autorisés : [a-z] [A-Z] [0-9]";
         errors.appendChild(error);
         e.preventDefault();
     }
@@ -32,5 +41,82 @@ addProjectForm.addEventListener("submit", function(e)
         errors.appendChild(error);
         e.preventDefault();
     } 
+    else if (inputSummary.value.trim().length >= 100)
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Résumé du projet' doit contenir au maximum 100 caractères";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputDescription.value.trim() === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Description du projet' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    } 
+    else if (inputDescription.value.trim().length <= 200) 
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Description du projet' doit contenir au moins 200 caractères.";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputTimeWeek.value === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Technologies utilisées sur ce projet' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputProjectImage.value === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Image de la technologie principale' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputDuration.value === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Durée estimée du projet' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputStudyLevel.value === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Niveau d'études souhaité des participants' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputNeed.value.trim() === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Besoin du projet' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    } 
+    else if (inputDescription.value.trim().length <= 100) 
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Besoin du projet' doit contenir au moins 200 caractères.";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
+
+    if (inputTimeWeek.value === "")
+    {
+        let error = document.createElement("p");
+        error.textContent = "Le champ 'Estimation du temps à consacrer au projet par semaine (par participant)' ne peut pas être vide";
+        errors.appendChild(error);
+        e.preventDefault();
+    }
 }
 )
