bbf63cce6ee0fd8e005f763f6521dd9aafd9312f
dylanejosse <dylane.josse@oclock.school>
2023-09-21 21:12:12 +0200

    Add target blank for use policy link

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -28,7 +28,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
                 'label_html' => true,
-                'label' => "J'accepte les <a href='conditions-utilisation'>conditions d'utilisation</a> Edudev", 
+                'label' => "J'accepte les <a target='_blank' class='text-decoration-none' href='conditions-utilisation'>conditions d'utilisation <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-up-right-square' viewBox='0 0 16 16'>
+                <path fill-rule='evenodd' d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z'/>
+              </svg></a> Edudev", 
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
